e0d7b51470a84032bc983fe45c70f0c42fab06af
Shakti <kashyapshakti1994@gmail.com>
2022-12-26 00:48:30 +0100

    last commit

--- books/api/urls.py
+++ books/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 
 router  = DefaultRouter()
-router.register('crud',views.BookViewSet, basename='book')
+router.register('bookdatabase',views.BookViewSet, basename='book')
 
 
 urlpatterns = [
